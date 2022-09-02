c15c4187f96127855c1de11c0555cc8860567152
Utkarsh <utkarsh.gupta1@housing.com>
2022-09-02 22:16:33 +0530

    Day61: React Component, Props, Map

--- react/Day60/my-app/src/App.js
+++ react/Day60/my-app/src/App.js
@@ -1,21 +1,54 @@
+import React from "react";
 import "./App.css";
 
-function App() {
+const MyApp = ({ welcomeMessage }) => {
+  const arr = [
+    {
+      id: "ijdiewd",
+      name: "Utkarsh",
+      age: 24,
+      city: "Agra",
+      address: "Uttar Pradesh, India",
+      lname: "Gupta",
+    },
+    {
+      id: "fejfeijo",
+      name: "Vivek",
+      age: 25,
+      city: "Mumbai",
+      address: "Maharashtra, India",
+      lname: "Jain",
+    },
+  ];
+  const title = welcomeMessage;
+
+  const getFullName = (item) => {
+    return item.name + " " + item.lname;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Utkarsh First application</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Header</h1>
       </header>
+      <main>
+        <h2 title={title}>{welcomeMessage}</h2>
+        <h3>
+          {arr.map((item) => {
+            return (
+              <div key={item.id} className="card">
+                <h3>{getFullName(item)}</h3>
+                <span>
+                  {item.age} {item.city} {item.address}
+                </span>
+              </div>
+            );
+          })}
+        </h3>
+      </main>
+      <footer className="footer">I love React</footer>
     </div>
   );
-}
+};
 
-export default App;
+export default MyApp;
--- react/Day60/my-app/src/index.js
+++ react/Day60/my-app/src/index.js
@@ -4,4 +4,4 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<App welcomeMessage="Welcome to Utkarsh's React class" />);
